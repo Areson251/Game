e41d66f50da390431a9a55d8fed780ae96cdae20
Areson <rektf12@gmail.com>
2021-06-02 00:07:01 +0400

    added enemies fixed boundaries of map added new sceens

---
+++ PlayerCharacter.py
@@ -0,0 +1,53 @@
+import arcade
+
+
+CHARACTER_SCALING = 1
+RIGHT_FACING = 0
+LEFT_FACING = 1
+
+
+class PlayerCharacter(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.character_face_direction = RIGHT_FACING
+        self.scale = CHARACTER_SCALING
+
+        main_path = "images/player/"
+        self.idle_texture_pair = self.load_texture_pair(f"{main_path}player_stand_right.png")
+        self.walk_textures = self.load_texture_pair(f"{main_path}player_stand_right.png")
+        self.fly_textures = self.load_texture_pair(f"{main_path}player_fly_right.png")
+        self.fall_textures = self.load_texture_pair(f"{main_path}player_fall_right.png")
+
+    def load_texture_pair(self, filename):
+        """
+        Load a texture pair, with the second being a mirror image.
+        """
+        return [
+            arcade.load_texture(filename),
+            arcade.load_texture(filename, mirrored=True)
+        ]
+
+
+    def update_animation(self, delta_time: float = 1 / 60):
+        # Figure out if we need to flip face left or right
+        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
+            self.character_face_direction = LEFT_FACING
+        elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
+            self.character_face_direction = RIGHT_FACING
+
+        # Idle animation
+        if self.change_x == 0 and self.change_y == 0:
+            self.texture = self.idle_texture_pair[self.character_face_direction]
+            return
+
+        # Walking
+        self.texture = self.walk_textures[self.character_face_direction]
+
+        # Flying and falling
+        if self.change_y > 0: #and not self.is_on_ladder:
+            self.texture = self.fly_textures[self.character_face_direction]
+            return
+        elif self.change_y < 0: #and not self.is_on_ladder:
+            self.texture = self.fall_textures[self.character_face_direction]
+            return
--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import arcade
+import arcade, PlayerCharacter, time
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Platformer"
+SCREEN_TITLE = "Platformer_course-work"
 
 # Constants used to scale our sprites from their original size
-CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 COIN_SCALING = 0.5
 SPRITE_PIXEL_SIZE = 128
 GRID_PIXEL_SIZE = (SPRITE_PIXEL_SIZE * TILE_SCALING)
 
-PLAYER_MOVEMENT_SPEED = 15
 GRAVITY = 1
-PLAYER_JUMP_SPEED = 1
+PLAYER_JUMP_SPEED = 20
+PLAYER_MOVEMENT_SPEED = 15
 
 # How many pixels to keep as a minimum margin between the character
 # and the edge of the screen.
@@ -22,51 +21,15 @@ RIGHT_VIEWPORT_MARGIN = 250
 BOTTOM_VIEWPORT_MARGIN = 50
 TOP_VIEWPORT_MARGIN = 100
 
-RIGHT_FACING = 0
-LEFT_FACING = 1
-
 MOVEMENT_SPEED = 5
 UPDATES_PER_FRAME = 7
 
-PLAYER_START_X = 150
-PLAYER_START_Y = 100
+PLAYER_START_X = 125
+PLAYER_START_Y = 200
 
-def load_texture_pair(filename):
-    """
-    Load a texture pair, with the second being a mirror image.
-    """
-    return [
-        arcade.load_texture(filename),
-        arcade.load_texture(filename, mirrored=True)
-    ]
-
-
-class PlayerCharacter(arcade.Sprite):
-    def __init__(self):
-        super().__init__()
-
-        self.character_face_direction = RIGHT_FACING
-        self.scale = CHARACTER_SCALING
-
-        main_path = "images/player/"
-        self.idle_texture_pair = load_texture_pair(f"{main_path}player_stand_right.png")
-        self.walk_textures = load_texture_pair("images/player/player_stand_right.png")
-
-
-    def update_animation(self, delta_time: float = 1 / 60):
-        # Figure out if we need to flip face left or right
-        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
-            self.character_face_direction = LEFT_FACING
-        elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
-            self.character_face_direction = RIGHT_FACING
-
-        # Idle animation
-        if self.change_x == 0 and self.change_y == 0:
-            self.texture = self.idle_texture_pair[self.character_face_direction]
-            return
-
-        # Walking
-        self.texture = self.walk_textures[self.character_face_direction]
+SPRITE_SCALING = 0.5
+SPRITE_NATIVE_SIZE = 128
+SPRITE_SIZE = int(SPRITE_NATIVE_SIZE * SPRITE_SCALING)
 
 
 class MyGame(arcade.Window):
@@ -81,25 +44,22 @@ class MyGame(arcade.Window):
         arcade.set_background_color((0, 150, 255))
         # Level
         self.level = 1
-        self.player_sprite = PlayerCharacter()
+        self.time_pause = 0
+        self.game_over = None
+        self.player_sprite = PlayerCharacter.PlayerCharacter()
 
     def setup(self, level):
         # Инициализируем три объекта: игрок, стены, монеты
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
         self.coin_list = arcade.SpriteList(use_spatial_hash=True)
+        self.enemy_list = arcade.SpriteList()
 
-        # Load textures for walking
-        self.walk_textures = []
-        texture = load_texture_pair("images/player/player_stand_right.png")
-        self.walk_textures.append(texture)
-
-        self.player_sprite.center_x = 150
-        self.player_sprite.center_y = 100
+        self.player_sprite.center_x = PLAYER_START_X
+        self.player_sprite.center_y = PLAYER_START_Y
         self.player_list.append(self.player_sprite)
 
         # Name of the layer in the file that has our platforms/walls
-
         platforms_layer_name = 'Platforms'
         coins_layer_name = 'Coins'
         self.dont_touch_list = None
@@ -123,34 +83,88 @@ class MyGame(arcade.Window):
                                                                 self.wall_list,
                                                                          GRAVITY)
 
+        # -- Draw a enemy1 on the platform
+        enemy1 = arcade.Sprite(":resources:images/enemies/wormGreen.png", SPRITE_SCALING)
+        enemy1.bottom = SPRITE_SIZE *3.2
+        enemy1.left = SPRITE_SIZE * 3
+        # Set boundaries on the left/right the enemy can't cross
+        enemy1.boundary_right = SPRITE_SIZE * 6
+        enemy1.boundary_left = SPRITE_SIZE * 3
+        enemy1.change_x = 2
+        self.enemy_list.append(enemy1)
+
+        # -- Draw a enemy2 on the platform
+        enemy2 = arcade.Sprite(":resources:images/enemies/wormGreen.png", SPRITE_SCALING)
+        enemy2.bottom = SPRITE_SIZE *14.2
+        enemy2.left = SPRITE_SIZE * 7
+        # Set boundaries on the left/right the enemy can't cross
+        enemy2.boundary_right = SPRITE_SIZE * 10
+        enemy2.boundary_left = SPRITE_SIZE * 7
+        enemy2.change_x = 4
+        self.enemy_list.append(enemy2)
+
+        # -- Draw a enemy2 on the platform
+        enemy3 = arcade.Sprite(":resources:images/enemies/wormGreen.png", SPRITE_SCALING)
+        enemy3.bottom = SPRITE_SIZE *12.2
+        enemy3.left = SPRITE_SIZE * 16
+        # Set boundaries on the left/right the enemy can't cross
+        enemy3.boundary_right = SPRITE_SIZE * 19
+        enemy3.boundary_left = SPRITE_SIZE * 16
+        enemy3.change_x = 1
+        self.enemy_list.append(enemy3)
+
+        # -- Draw a enemy2 on the platform
+        enemy4 = arcade.Sprite(":resources:images/enemies/wormGreen.png", SPRITE_SCALING)
+        enemy4.bottom = SPRITE_SIZE *5.2
+        enemy4.left = SPRITE_SIZE * 10
+        # Set boundaries on the left/right the enemy can't cross
+        enemy4.boundary_right = SPRITE_SIZE * 13
+        enemy4.boundary_left = SPRITE_SIZE * 10
+        enemy4.change_x = 3
+        self.enemy_list.append(enemy4)
+
+        # -- Draw a enemy2 on the platform
+        enemy5 = arcade.Sprite(":resources:images/enemies/wormGreen.png", SPRITE_SCALING)
+        enemy5.bottom = SPRITE_SIZE *2.2
+        enemy5.left = SPRITE_SIZE * 7
+        # Set boundaries on the left/right the enemy can't cross
+        enemy5.boundary_right = SPRITE_SIZE * 30
+        enemy5.boundary_left = SPRITE_SIZE * 7
+        enemy5.change_x = 5
+        self.enemy_list.append(enemy5)
+
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
-
-    def on_draw(self):
-        """ Отрендерить этот экран. """
-        arcade.start_render()
-        # Здесь код рисунка
-        self.wall_list.draw()
-        self.coin_list.draw()
-        self.player_list.draw()
-        self.dont_touch_list.draw()
-
-        level_text = f"Level: {self.level}"
-        score_text = f"Score: {self.score}"
-        arcade.draw_text(level_text, 300 + self.view_left, 550 + self.view_bottom,
-                         arcade.csscolor.WHITE, 30)
-        arcade.draw_text(score_text, 450 + self.view_left, 550 + self.view_bottom,
-                arcade.csscolor.WHITE, 30)
-        if self.level == 3:
-            end_text = f"!!!YOU WIN!!!"
-            arcade.draw_text(end_text, 250 + self.view_left, 300 + self.view_bottom,
-                             arcade.csscolor.BLACK, 50)
 
     def update(self, delta_time):
         # Move the player with the physics engine
         self.player_list.update_animation()
         self.physics_engine.update()
 
-        # See if we hit any coins
+        if self.game_over:
+            time.sleep(3)
+            self.game_over = False
+
+        # Update the player based on the physics engine
+        if not self.game_over:
+            # Move the enemies
+            self.enemy_list.update()
+
+        # Check each enemy
+        for enemy in self.enemy_list:
+            # If the enemy hit a wall, reverse
+            if len(arcade.check_for_collision_with_list(enemy, self.wall_list)) > 0:
+                enemy.change_x *= -1
+            # If the enemy hit the left boundary, reverse
+            elif enemy.boundary_left is not None and enemy.left < enemy.boundary_left:
+                enemy.change_x *= -1
+            # If the enemy hit the right boundary, reverse
+            elif enemy.boundary_right is not None and enemy.right > enemy.boundary_right:
+                enemy.change_x *= -1
+
+        # Track if we need to change the viewport
+        changed_viewport = False
+
+        # See if player hit any coins
         coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
         # Loop through each coin we hit (if any) and remove it
         for coin in coin_hit_list:
@@ -158,60 +172,42 @@ class MyGame(arcade.Window):
             coin.remove_from_sprite_lists()
             self.score += 1
                 # arcade.play_sound(self.collect_coin_sound)
-        changed_viewport = False
 
-        if self.player_sprite.center_y < -100:
-            self.player_sprite.center_x = PLAYER_START_X
-            self.player_sprite.center_y = PLAYER_START_Y
-
-            # Set the camera to the start
-            self.view_left = 0
-            self.view_bottom = 0
-            changed_viewport = True
+        # See if the player hit a worm. If so, game over.
+        if len(arcade.check_for_collision_with_list(self.player_sprite, self.enemy_list)) > 0:
+            self.game_over = True
+        # Track if player gone away from the map
+        if self.player_sprite.center_x > 1800 or self.player_sprite.center_x < -150 or self.player_sprite.center_y > 1500:
+            self.game_over = True
+        # Did the player touch something they should not?
+        if arcade.check_for_collision_with_list(self.player_sprite, self.dont_touch_list):
+            self.game_over = True
 
         # --- Manage Scrolling ---
-        # Track if we need to change the viewport
-        changed = False
         # Scroll left
         left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
         if self.player_sprite.left < left_boundary:
             self.view_left -= left_boundary - self.player_sprite.left
-            changed = True
+            changed_viewport = True
         # Scroll right
         right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
         if self.player_sprite.right > right_boundary:
             self.view_left += self.player_sprite.right - right_boundary
-            changed = True
+            changed_viewport = True
         # Scroll up
         top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
         if self.player_sprite.top > top_boundary:
             self.view_bottom += self.player_sprite.top - top_boundary
-            changed = True
+            changed_viewport = True
         # Scroll down
         bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
         if self.player_sprite.bottom < bottom_boundary:
             self.view_bottom -= bottom_boundary - self.player_sprite.bottom
-            changed = True
-        if changed:
-            # Only scroll to integers. Otherwise we end up with pixels that
-            # don't line up on the screen
-            self.view_bottom = int(self.view_bottom)
-            self.view_left = int(self.view_left)
-            # Do the scrolling
-            arcade.set_viewport(self.view_left,
-                                SCREEN_WIDTH + self.view_left,
-                                self.view_bottom,
-                                SCREEN_HEIGHT + self.view_bottom)
+            changed_viewport = True
 
-        # Did the player touch something they should not?
-
-        if arcade.check_for_collision_with_list(self.player_sprite,
-                                                self.dont_touch_list):
-            self.player_sprite.change_x = 0
-            self.player_sprite.change_y = 0
+        if self.game_over:
             self.player_sprite.center_x = PLAYER_START_X
             self.player_sprite.center_y = PLAYER_START_Y
-
             # Set the camera to the start
             self.view_left = 0
             self.view_bottom = 0
@@ -229,6 +225,49 @@ class MyGame(arcade.Window):
             self.view_left = 0
             self.view_bottom = 0
             changed_viewport = True
+
+        if changed_viewport:
+            # Only scroll to integers. Otherwise we end up with pixels that
+            # don't line up on the screen
+            self.view_bottom = int(self.view_bottom)
+            self.view_left = int(self.view_left)
+            # Do the scrolling
+            arcade.set_viewport(self.view_left,
+                                SCREEN_WIDTH + self.view_left,
+                                self.view_bottom,
+                                SCREEN_HEIGHT + self.view_bottom)
+
+
+    def on_draw(self):
+        """ Отрендерить этот экран. """
+        arcade.start_render()
+        # Здесь код рисунка
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.enemy_list.draw()
+        self.player_list.draw()
+        self.dont_touch_list.draw()
+
+        level_text = f"Level: {self.level}"
+        score_text = f"Score: {self.score}"
+        arcade.draw_text(level_text, 300 + self.view_left, 550 + self.view_bottom,
+                         arcade.csscolor.WHITE, 30)
+        arcade.draw_text(score_text, 450 + self.view_left, 550 + self.view_bottom,
+                arcade.csscolor.WHITE, 30)
+
+        if self.game_over:
+            end_text = f"GAME OVER"
+            arcade.draw_text(end_text, 250 + self.view_left, 300 + self.view_bottom,
+                             arcade.csscolor.BLACK, 50)
+            end_text = f"you will start over in 3 seconds"
+            arcade.draw_text(end_text, 185 + self.view_left, 250 + self.view_bottom,
+                             arcade.csscolor.BLACK, 30)
+
+        if self.level == 3:
+            end_text = f"!!!YOU WIN!!!"
+            arcade.draw_text(end_text, 250 + self.view_left, 300 + self.view_bottom,
+                             arcade.csscolor.BLACK, 50)
+
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
